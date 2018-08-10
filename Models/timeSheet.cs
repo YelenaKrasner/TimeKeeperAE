c05791f9b75b6a1882651c4801b4996145c186a3
YelenaKrasner <yelena.krasner@mavenwave.com>
2018-08-10 19:40:52 +0000

    First Commit

---
+++ Models/timeSheet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TimeKeeper.Models
+{
+    public class timeSheet
+    {
+        public string name { get; set; }
+        public string department { get; set; }
+        public string projectName { get; set; }
+        public DateTime timeIn { get; set; }
+        public DateTime timeOut { get; set; }
+    }
+}
